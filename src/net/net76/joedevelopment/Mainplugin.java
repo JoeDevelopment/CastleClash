66391943d28167f398b9b6c236526f1a444b9e53
JoeDevelopment <admin@joedevelopment.net76.net>
2014-10-05 08:32:14 -0500

    Bug fixing

--- src/net/net76/joedevelopment/Mainplugin.java
+++ src/net/net76/joedevelopment/Mainplugin.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,13 +25,13 @@ public class Mainplugin extends JavaPlugin {
 		getServer().getPluginManager().registerEvents(new Listener() {
 			@EventHandler
 			public void onPlayerInteract(PlayerInteractEvent e) {
-				if(e.getClickedBlock() != null) {
-					if(e.getClickedBlock().getType() == Material.SIGN) {
-						Sign s = (Sign)e.getClickedBlock();
+				if(e.getClickedBlock() != null && e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+					if(e.getClickedBlock().getType().equals(Material.SIGN_POST) || e.getClickedBlock().getType().equals(Material.WALL_SIGN)) {
+						Sign s = (Sign)e.getClickedBlock().getState();
 						if(s.getLine(0).equalsIgnoreCase("[JOINGAME]")) {
 							e.getPlayer().sendMessage("This has not been implemented yet.");
 						}
-					} else if(e.getClickedBlock().getType() == Material.OBSIDIAN) {
+					} else if(e.getClickedBlock().getType().equals(Material.OBSIDIAN)) {
 						// Check coords, Implement later
 					}
 				}
